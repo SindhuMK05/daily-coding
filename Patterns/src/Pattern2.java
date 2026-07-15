c7c0279823253196ed7563425dfcc425b97660fd
Sindhumkadapatti <sindhukadapatti29@gmai.com>
2026-07-15 19:48:28 +0530

    Pattern 2 Update

--- Patterns/src/Pattern2.java
+++ Patterns/src/Pattern2.java
@@ -1,2 +1,11 @@
 public class Pattern2 {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) { //outer for loop (rows)
+            for (int j = 1; j <= 5 - i; j++) { // inner for loop(columns)
+                System.out.print("*");
+            }
+            System.out.println("* ");
+        }
+
+    }
 }
